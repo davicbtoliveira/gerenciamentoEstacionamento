47769f9fd9f422bf2ecfeb2ce169ab0c67760ef5
DBranco <davicbtoliveira@hotmail.com>
2025-04-13 22:40:51 -0400

    adding repositories

--- src/main/java/com/dcbto/estacionamento/repositories/EmpresaRepository.java
+++ src/main/java/com/dcbto/estacionamento/repositories/EmpresaRepository.java
@@ -2,6 +2,8 @@ package com.dcbto.estacionamento.repositories;
 
 import com.dcbto.estacionamento.entities.Empresas;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmpresaRepository extends JpaRepository<Empresas, Long> {
 }
--- src/main/java/com/dcbto/estacionamento/repositories/VeiculoRepository.java
+++ src/main/java/com/dcbto/estacionamento/repositories/VeiculoRepository.java
@@ -2,6 +2,8 @@ package com.dcbto.estacionamento.repositories;
 
 import com.dcbto.estacionamento.entities.Veiculos;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface VeiculoRepository extends JpaRepository<Veiculos, Long> {
 }
